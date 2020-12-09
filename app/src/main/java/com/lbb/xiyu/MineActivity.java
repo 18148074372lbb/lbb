a3e96d3e786d4aeac09dc571434b682c3fdec730
lbb <2284496322qq.com>
2020-12-09 08:34:03 +0800

    校园洗浴项目

---
+++ app/src/main/java/com/lbb/xiyu/MineActivity.java
@@ -0,0 +1,14 @@
+package com.lbb.xiyu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mine);
+    }
+}
